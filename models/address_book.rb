e948358777cec1f6a12f3ec754a8ef4493c318d3
kba239 <adams.kate.b@gmail.com>
2016-12-03 20:17:47 -0500

    WIP Assignment 5 Address Bloc Models

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,11 +1,21 @@
 require_relative 'entry'
 
-  class AddressBook
-    attr_reader :entries
+class AddressBook
+  attr_reader :entries
 
-    def initialize
-      @entries = []
+  def initialize
+    @entries = []
+  end
+
+  def remove_entry(name, phone_number, email)
+    delete_entry = nil
+    entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        entry = delete_entry
+      end
     end
+    entries.delete(delete_entry)
+  end
 
   def add_entry(name, phone_number, email)
     index = 0
